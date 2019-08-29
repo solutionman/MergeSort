dd46a8f7ec22bd4a117b87e835907452a04146c1
lom <solutionman@inbox.ru>
2019-08-29 08:57:18 +0700

    jar file

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello, for merging files enter arguments: ");
+        System.out.println("[type of sort (-a -d)] [type of data (-s -i)] [output file(name)] [input files(names)] ");
+        System.out.println("for example:  -d -s out.txt in1.txt in2.txt ");
+    }
+}
