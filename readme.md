c31e60d6c5be9e3f6465d8980397f789760a017a
lom <solutionman@inbox.ru>
2019-08-30 09:22:31 +0700

    args

--- readme.md
+++ readme.md
@@ -5,3 +5,10 @@ cd target/
 
 java -jar MergeSort.jar
 
+for merging files enter arguments:
+[type of sort (-a -d)] [type of data (-s -i)] [output file(name)] [input files(names)]
+
+for example:  -d -s out.txt in1.txt in2.txt
+
+
+
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,8 +2,12 @@ package main;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello, for merging files enter arguments: ");
-        System.out.println("[type of sort (-a -d)] [type of data (-s -i)] [output file(name)] [input files(names)] ");
-        System.out.println("for example:  -d -s out.txt in1.txt in2.txt ");
+
+        String message = args.length > 0 ? "Your arguments: " : "No arguments";
+        System.out.println( message );
+        for( String arg : args ){
+            System.out.println( arg );
+        }
+
     }
 }
