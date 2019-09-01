707d9fd1e7eaa01937ed6f5ea167502ec1dfd885
lom <solutionman@inbox.ru>
2019-09-01 12:07:00 +0700

    check file

--- src/main/java/main/CheckArguments.java
+++ src/main/java/main/CheckArguments.java
@@ -1,16 +1,27 @@
 package main;
 
+import java.io.File;
 import java.util.ArrayList;
 
 class CheckArguments {
     boolean checkArguments(String typeOfSort, String typeOfData, String outputFileName, ArrayList<String> inputFilesName){
+
         if( !typeOfSort.equals("-a") && !typeOfSort.equals("-d") ){
             System.out.println("Первый аргумент должен иметь значение -a или -d");
             return false;
         }
+
         if( !typeOfData.equals("-i") && !typeOfData.equals("-s") ){
             System.out.println("Второй аргумент должен иметь значиние -i или -s");
             return false;
+        }
+
+        for( String ifn : inputFilesName ){
+            File f = new File(ifn);
+            if( !f.exists() || f.isDirectory() ){
+                System.out.println( "Нет файла " +  ifn + " или он не является файлом");
+                return false;
+            }
         }
 
         return true;
