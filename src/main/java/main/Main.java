cd4899dacc17347cd916db2c4863614b78ce36cd
lom <solutionman@inbox.ru>
2019-09-03 08:04:45 +0700

    MergingSorting

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -19,29 +16,14 @@ public class Main{
         String outputFileName = args[2];
         ArrayList<String> inputFileName = new ArrayList<>(Arrays.asList(args).subList(3, args.length));
 
-//        System.out.print( typeOfSort + " " + typeOfData + " " + outputFileName + " ");
-//        for(String a : inputFileName){
-//            System.out.print(a + " ");
-//        }
-
         CheckArguments checkArgs = new CheckArguments();
         if(!checkArgs.checkArguments( typeOfSort, typeOfData, outputFileName, inputFileName )){
             System.out.println("Ошибка в аргументах, работа программы прекращена.");
             System.exit(0);
         }
 
+        MergingSorting mergingSorting = new MergingSorting();
+        mergingSorting.mergeAndSort( inputFileName.get( 0 ), inputFileName.get( 1 ) );
 
-        try(
-            BufferedReader reader1 = new BufferedReader( new FileReader( inputFileName.get( 0 ) ) );
-            BufferedReader reader2 = new BufferedReader( new FileReader( inputFileName.get( 1 ) ) )
-        ){
-            String line1;
-            String line2;
-            while( (line1 = reader1.readLine() ) != null && (line2 = reader2.readLine() ) != null ){
-                System.out.println(line1 + " " + line2);
-            }
-        } catch( IOException e ){
-            System.out.println("Ошибка при чтении файла " + inputFileName.get( 0 ));
-        }
     }
 }
---
+++ src/main/java/main/MergingSorting.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+class MergingSorting{
+     void mergeAndSort(String firstFile, String secondFile){
+
+        try(
+                BufferedReader reader1 = new BufferedReader( new FileReader( firstFile ) );
+                BufferedReader reader2 = new BufferedReader( new FileReader( secondFile ) )
+        ){
+            String line1;
+            String line2;
+            while( (line1 = reader1.readLine() ) != null && (line2 = reader2.readLine() ) != null ){
+                System.out.println(line1 + " " + line2);
+            }
+        } catch( IOException e ){
+            System.out.println("Ошибка при чтении файла " + e);
+        }
+
+    }
+}
