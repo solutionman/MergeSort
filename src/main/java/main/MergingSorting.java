8efe1f1d15caad304cfcee7eaf9a0a4f0b3ba324
lom <solutionman@inbox.ru>
2019-09-03 09:06:48 +0700

    writing to file

--- src/main/java/main/MergingSorting.java
+++ src/main/java/main/MergingSorting.java
@@ -1,7 +1,9 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 class MergingSorting{
@@ -9,12 +11,22 @@ class MergingSorting{
 
         try(
                 BufferedReader reader1 = new BufferedReader( new FileReader( firstFile ) );
-                BufferedReader reader2 = new BufferedReader( new FileReader( secondFile ) )
+                BufferedReader reader2 = new BufferedReader( new FileReader( secondFile ) );
+                BufferedWriter writer = new BufferedWriter( new FileWriter( outputFile ) )
         ){
             String line1;
             String line2;
             while( (line1 = reader1.readLine() ) != null && (line2 = reader2.readLine() ) != null ){
                 System.out.println(line1 + " " + line2);
+                int first = Integer.parseInt( line1 );
+                int second = Integer.parseInt( line2 );
+                if( first > second){
+                    writer.write( Integer.toString(second) );
+                    writer.write( Integer.toString(first) );
+                } else {
+                    writer.write( Integer.toString(first) );
+                    writer.write( Integer.toString(second) );
+                }
             }
         } catch( IOException e ){
             System.out.println("Ошибка при чтении файла " + e);
