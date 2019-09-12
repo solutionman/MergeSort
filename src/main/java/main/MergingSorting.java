a2954c0bfee7633a4dde6cd7861e5afc07ec7934
lom <solutionman@inbox.ru>
2019-09-12 09:33:16 +0700

    merging

--- src/main/java/main/MergingSorting.java
+++ src/main/java/main/MergingSorting.java
@@ -14,39 +14,62 @@ class MergingSorting{
                 BufferedReader reader2 = new BufferedReader( new FileReader( secondFile ) );
                 BufferedWriter writer = new BufferedWriter( new FileWriter( outputFile ) )
         ){
-            String line1;
-            String line2;
-            while( (line1 = reader1.readLine() ) != null && (line2 = reader2.readLine() ) != null ){
+            String line1 = reader1.readLine();
+            String line2 = reader2.readLine();
+            while( line1 != null || line2 != null ){
                 System.out.println(line1 + " " + line2);
 
-                if(typeOfData.equals( "-i" )){
-                    int first = Integer.parseInt( line1 );
-                    int second = Integer.parseInt( line2 );
-                    if( first > second){
-                        writer.write( Integer.toString(second) );
-                        writer.newLine();
-                        writer.write( Integer.toString(first) );
-                        writer.newLine();
-                    } else {
-                        writer.write( Integer.toString(first) );
-                        writer.newLine();
-                        writer.write( Integer.toString(second) );
-                        writer.newLine();
+                if( line1 != null && line2 != null )
+                {
+                    if( typeOfData.equals( "-i" ) )
+                    {
+                        int first = Integer.parseInt( line1 );
+                        int second = Integer.parseInt( line2 );
+                        if( first > second )
+                        {
+                            writer.write( Integer.toString( second ) );
+                            writer.newLine();
+                            writer.write( Integer.toString( first ) );
+                            writer.newLine();
+                        }
+                        else
+                        {
+                            writer.write( Integer.toString( first ) );
+                            writer.newLine();
+                            writer.write( Integer.toString( second ) );
+                            writer.newLine();
+                        }
                     }
-                } else {
-                    if( line1.compareTo( line2 ) > 0 ){
-                        writer.write( line2 );
-                        writer.newLine();
-                        writer.write( line1 );
-                        writer.newLine();
-                    } else {
-                        writer.write( line1 );
-                        writer.newLine();
-                        writer.write( line2 );
-                        writer.newLine();
+                    else
+                    {
+                        if( line1.compareTo( line2 ) > 0 )
+                        {
+                            writer.write( line2 );
+                            writer.newLine();
+                            writer.write( line1 );
+                            writer.newLine();
+                        }
+                        else
+                        {
+                            writer.write( line1 );
+                            writer.newLine();
+                            writer.write( line2 );
+                            writer.newLine();
+                        }
                     }
                 }
 
+                if( line1 == null ){
+                    writer.write( line2 );
+                    writer.newLine();
+                }
+                if ( line2 == null ){
+                    writer.write( line1 );
+                    writer.newLine();
+                }
+
+                line1 = reader1.readLine();
+                line2 = reader2.readLine();
             }
         } catch( IOException e ){
             System.out.println("Ошибка при чтении файла " + e);
